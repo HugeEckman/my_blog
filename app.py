229893fa7ebed7e34cdbe4559014a51005f59f29
Дмитрий Евтушенко <user@user.com>
2023-12-24 16:21:24 +0300

    add register

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from model import User, Post, Tag, db 
 from flask_migrate import Migrate
-from forms import LoginForm
+from forms import LoginForm, RegistrationForm
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 # from werkzeug.urls import url_parse
 import config
@@ -64,6 +64,21 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.cli.command('create_db')
